4bb1be9473a98d006f503153d6180bd356d800f9
kanish <kanish2233@gmail.com>
2024-09-25 13:12:04 +0530

    Second commit

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eg-=6&2jnplwadze-ltkej5_(m^a%*afwh%3*dq2dll1&zobpp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
